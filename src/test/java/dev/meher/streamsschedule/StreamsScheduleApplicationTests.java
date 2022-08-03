b7330c150ae985f234856cd30c627791cc44fac1
Sai Meher Borra <sai.meher@kanerika.com>
2022-08-03 12:50:34 +0530

    adding files

---
+++ src/test/java/dev/meher/streamsschedule/StreamsScheduleApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.meher.streamsschedule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamsScheduleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
